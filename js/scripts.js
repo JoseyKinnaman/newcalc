7effb64816c9bbbf9859207429b22c9c1938d811
Jozy Kinnaman <jozypants@gmail.com>
2020-03-25 11:57:57 -0700

    add user interface and business logic and form to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number to add:"));
-var number2 = parseInt(prompt("Enter another number to add:"));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-alert(add(number1, number2));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var multiply = function(number3, number4) {
-  return number3 * number4;
-}
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-var number3 = parseInt(prompt("Enter a number to multiply"));
-var number4 = parseInt(prompt("Enter another number to multiply by:"));
+// Everything below this line is user interface (or front-end) logic:
 
-alert(multiply(number3, number4));
-
-var divide = function(number5, number6) {
-  return number5/number6;
-}
-
-var number5 = parseInt(prompt("Enter a number to divide"));
-var number6 = parseInt(prompt("Enter another number to divide by:"));
-
-alert(divide(number5, number6));
-
-var subtract = function(number7, number8) {
-  return number7-number8;
-}
-
-alert(subtract(number7, number8));
-
-var number = parseInt(prompt("Enter a number to subtract"));
-var number4 = parseInt(prompt("Enter another number to subtract by:"));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
