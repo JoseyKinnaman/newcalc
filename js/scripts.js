c1c821ca178248004f746ee81a4ad020026ee953
Jozy Kinnaman <jozypants@gmail.com>
2020-03-25 13:53:11 -0700

    add buttons. need to debug how to display multiple outputs.

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,34 @@ var subtract = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#num1").val());
+    console.log(number1);
+    var number2 = parseInt($("#num2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $(".output").text(result);
+    console.log(result);
+  });
+  $("form#calculator").submit(function(event) {
+    // event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var result = subtract(number1, number2);
+    $(".output").text(result);
+  });
+  $("form#calculator").submit(function(event) {
+    // event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var result = multiply(number1, number2);
+    $(".output").text(result);
+  });
+  $("form#calculator").submit(function(event) {
+    // event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var result = divide(number1, number2);
+    $(".output").text(result);
   });
 });
